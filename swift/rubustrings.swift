370659f98ebc328462360165664517aa88a50e21
David Cordero <dcorderoramirez@gmail.com>
2016-05-29 19:32:04 +0200

    Methods to remove comments and empty lines migrated to swift

--- swift/rubustrings.swift
+++ swift/rubustrings.swift
@@ -14,11 +14,46 @@ guard Process.arguments.count > 1 else {
     exit(0)
 }
 
+func openAndReadFile(fileName: String) -> String? {
+    do {
+        return try String(contentsOfFile: fileName, encoding: NSUTF16StringEncoding)
+    }
+    catch {
+        print("\n✘ Error reading file: \(fileName)".red)
+    }
+    return nil
+}
+
+func removeComments(fileData: String) -> String {
+    let multilineCommentsRegex = try! NSRegularExpression(pattern: "\\/\\*.*?\\*\\/", options: .CaseInsensitive)
+    return multilineCommentsRegex.stringByReplacingMatchesInString(fileData, options: [], range: NSRange(0..<fileData.utf16.count), withTemplate: "")
+    
+}
+
+func removeEmptyLines(fileData: String) -> String {
+    return fileData.componentsSeparatedByString("\n").filter { $0 != "" }.joinWithSeparator("\n")
+}
+
+func validateLocalizableStringFile(fileName: String) -> Bool {
+    
+    if let fileData = openAndReadFile(fileName) {
+        
+        let noComments = removeComments(fileData)
+        let noEmptyLinex = removeEmptyLines(noComments)
+        
+        print(noEmptyLinex)
+    }
+    
+    return false
+}
+
+
+
 var error = false
 for fileName in Process.arguments where fileName != Process.arguments.first {
     print("\nProcessing file: \(fileName)".blue)
     
-    if system("./rubustrings.rb \(fileName)") == 0 {
+    if validateLocalizableStringFile(fileName) {
         print("\nResult: ✓ Strings file validated succesfully".green)
     }
     else {
