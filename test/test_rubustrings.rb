8d8f73f9291e0a9731634793551e40e55b252c9a
David Cordero <david@corderoramirez.com>
2017-12-03 22:37:48 +0100

    Add tests

--- test/test_rubustrings.rb
+++ test/test_rubustrings.rb
@@ -23,4 +23,26 @@ class RubustringsTest < Minitest::Test
     assert_match output, out
     assert_empty err
   end
+
+  def test_localizable_beginning_mismatch
+    output = File.read('test/suites/Localizable_beginning_mismatch.out')
+
+    out, err = capture_io do
+      Rubustrings.validate(['test/suites/Localizable_beginning_mismatch.in'])
+    end
+
+    assert_match output, out
+    assert_empty err
+  end
+
+  def test_localizable_number_of_variables_mismatch
+    output = File.read('test/suites/Localizable_number_of_variables_mismatch.out')
+
+    out, err = capture_io do
+      Rubustrings.validate(['test/suites/Localizable_number_of_variables_mismatch.in'])
+    end
+
+    assert_match output, out
+    assert_empty err
+  end
 end
